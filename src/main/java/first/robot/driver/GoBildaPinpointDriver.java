cb136dab7c7729ea30fe87a2e7b0e985c88173e3
Sebastian Erives <serivesmejia@deltacv.org>
2026-05-07 09:57:05 -0600

    Fix sus license header

--- src/main/java/first/robot/driver/GoBildaPinpointDriver.java
+++ src/main/java/first/robot/driver/GoBildaPinpointDriver.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 // Ported from the FTC GoBILDA Pinpoint driver (MIT License, Base 10 Assets, LLC, 2025)
 
 package first.robot.driver;
